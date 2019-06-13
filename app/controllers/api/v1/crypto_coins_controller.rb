712e9d127739e682a9cbd5c221df380b94deceae
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-13 03:42:09 -0300

    Adding update_all method

--- app/controllers/api/v1/crypto_coins_controller.rb
+++ app/controllers/api/v1/crypto_coins_controller.rb
@@ -9,6 +9,16 @@ module Api
           data: list_crypto_coin
         }, status: :ok
       end
+      def update_all
+        currency = params[:currency] ? params[:currency] : "BRL"
+        CryptocurrencyService.new(currency).update_crypto_coins
+        list_crypto_coin = CryptoCoin.all
+        render json: {
+          status: 'SUCCESS',
+          message: 'CryptoCoin list updated',
+          data: list_crypto_coin
+        }, status: :ok
+      end
     end
   end
 end
