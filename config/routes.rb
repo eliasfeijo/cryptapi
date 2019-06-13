d60ba31964f8c223889c8beac9b88a319e826a09
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-13 03:42:37 -0300

    Adding PUT route to /api/v1/crypto_coins

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get '/crypto_coins', to: 'crypto_coins#index'
+      put '/crypto_coins', to: 'crypto_coins#update_all'
     end
   end
 end
