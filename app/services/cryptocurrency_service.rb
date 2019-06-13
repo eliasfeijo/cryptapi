eea5fd55044428d3f78e5a305e11175468f0cbdd
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-13 03:23:23 -0300

    Moving update_crypto_coins logic to cryptocurrency_service

--- app/services/cryptocurrency_service.rb
+++ app/services/cryptocurrency_service.rb
@@ -17,4 +17,29 @@ class CryptocurrencyService
     res = http.request(req)
     res.body
   end
+  def update_crypto_coins
+    response_json = get_cryptocurrency_list
+    data = JSON.parse(response_json)["data"]
+
+    if data.nil? then
+      raise "Bad response:\n #{response_json}"
+    end
+
+    if CryptoCoin.first then
+      puts "Updating current database"
+      data.each do |coin|
+        title = coin["name"]
+        price = coin["quote"][@currency]["price"]
+        crypto_coin = CryptoCoin.find_by(title: title)
+        crypto_coin.update(price: price)
+      end
+    else
+      puts "Populating CryptoCoin table"
+      data.each do |coin|
+        title = coin["name"]
+        price = coin["quote"][@currency]["price"]
+        crypto_coin = CryptoCoin.create(title: title, price: price)
+      end
+    end
+  end
 end
